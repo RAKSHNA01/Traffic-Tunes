569635691437aa9b75323610246538a9cd61ba52
Rakshna.R <25f2003109@ds.study.iitm.ac.in>
2025-12-22 18:13:52 +0530

    Add traffic level functions to update result text

---
+++ script.js
@@ -0,0 +1,14 @@
+function lowTraffic() {
+  document.getElementById("result").innerText =
+    "Low Traffic 🚗 | Slow Xylophone Music Playing 🎶";
+}
+
+function mediumTraffic() {
+  document.getElementById("result").innerText =
+    "Medium Traffic 🚙🚗 | Moderate Xylophone Music 🎶";
+}
+
+function highTraffic() {
+  document.getElementById("result").innerText =
+    "High Traffic 🚗🚗🚗 | Fast & High-Pitch Xylophone 🎶";
+}
